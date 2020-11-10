113d08c9032ae3dfb26726ba6bd6fd0a51525a8f
re-vc <frederikrehovic@gmail.com>
2020-11-10 09:27:49 +0100

    initial

---
+++ src/Mike/Main.java
@@ -0,0 +1,7 @@
+package Mike;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Rade/Main.java
@@ -0,0 +1,7 @@
+package Rade;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
